6e45c4aea44bfd3d72dacdfc46ca4febdefd3d18
GCambridge <gavrielcambridge@gmail.com>
2024-07-01 15:59:07 -0400

    Fixed Date Times
    plot includes date and time.
    testing the addition of the calibration file.

--- app.R
+++ app.R
@@ -12,9 +12,14 @@ WQlist <- c("Select", "Conductivity", "Temperature", "Turbidity", "Depth",
 Towers <- c("Select", "1", "2")
 
 #Load in data file - seems like it can be from a URL
-Tx15 <- read.csv("BMPTripod1_Min_15.dat", stringsAsFactors = FALSE)
-Tx15$TIMESTAMP <- as.Date(Tx15$TIMESTAMP)
+Tx15 <- read.csv("BMPTripod1_Min_15.dat")
+Tx15$TIMESTAMP <- ymd_hms(Tx15$TIMESTAMP)
+
+CalData <- read.csv("TestCalData.csv")
+CalData$TIMESTAMP <- ymd_hms(CalData$TIMESTAMP)
+
 Tx15flt <- Tx15
+CalDataflt <- CalData
 
 # User Interface Definition 
 ui <- fluidPage(
@@ -39,8 +44,8 @@ ui <- fluidPage(
                   "Tower 2" = "t2")),
       
       #Date Time Selector
-      textInput("fromDate", "From:", value = "2024-05-13" ),
-      textInput("toDate", "To:", value = "2024-06-01" ),
+      textInput("fromDate", "From:", value = "2024-02-02 00:00:00" ),
+      textInput("toDate", "To:", value = "2024-05-01 00:00:00" ),
          
       #1st Parameter
       selectInput("WQP1", "Water Quality Parameter 1:", WQlist), 
@@ -59,11 +64,11 @@ ui <- fluidPage(
         tabPanel("Table", tableOutput("VarTable")), 
         tabPanel("Camera Feed",imageOutput("")), 
         tabPanel("Calibration Data", 
-                 # fluidRow(
-                 #   column(width = 4,
-                 #     fluidRow(
-                 #       fileInput("file", "Calibration File:")),
-                 #   )),
+                 fluidRow(
+                   column(width = 4,
+                       fileInput("file", "Calibration File:")),
+                        actionButton("plotCalib", "Plot Calibration Data"),
+                   ),
                   plotOutput("calib"))
       )
     )
@@ -78,18 +83,23 @@ server <- shinyServer(function(input, output, session) {
 #Filter to selected date range:
   rangeDates <- reactive({
     Tx15flt <- filter(Tx15, between(Tx15$TIMESTAMP, 
-                        as.Date(input$fromDate),
-                        as.Date(input$toDate)
+                        ymd_hms(input$fromDate),
+                        ymd_hms(input$toDate)
+                      ))
+    
+    CalDataflt <- filter(CalData, between(CalData$TIMESTAMP, 
+                        ymd_hms(input$fromDate),
+                        ymd_hms(input$toDate)
                       ))
     ggplot() +
-      geom_point(data = Tx15flt, aes(x=TIMESTAMP, y = NO3eq)) +
-      # geom_point(data = CalibData, aes(x =TIMESTAMP, y = NO3eq))+
-      # scale_x_date(date_labels = "%b-%d-%Y") +
+      geom_line(data = Tx15flt, aes(x=TIMESTAMP, y = NO3eq)) +
+      geom_line(data = CalDataflt, aes(x=TIMESTAMP, y = NO3conc), color = "red") +
+    
       labs(title = "Nitrate Calibration Data at Station X") +
       theme_classic()
     
                 })|> 
-    bindEvent(input$action, ignoreNULL = FALSE)
+    bindEvent(input$plotCalib, ignoreNULL = FALSE)
  
   
   output$calib <- renderPlot({
