97567813c2f24b242b07f6184fc5a7c48f24f2c6
GCambridge <gavrielcambridge@gmail.com>
2024-07-05 11:35:40 -0400

    Function parameter selector
    allows for any two variables to be selected and plotted against each other in the "plots" tab.

--- app.R
+++ app.R
@@ -2,12 +2,12 @@ library(shiny)
 library(ggplot2)
 library(lubridate)
 library(haven)
+library(dplyr)
 
 # list of water quality parameters to plot
-WQlist <- c("Select", "Conductivity", "Temperature", "Turbidity", "Depth", 
-            "Nitrate", "Total Suspended Solids", "Total Organic Carbon", 
-            "Dissolved Organic Carbon", "Soil Water Content", "Soil Conductivity",
-            "Soil Temperature")
+WQlist <- list("TIMESTAMP", "Conductivity", "Temperature", "Turbidity", "Depth", 
+            "TSSeq", "TOCeq","DOCeq","NO3eq","Temp_spv3")
+
 #List of monitoring towers
 Towers <- c("Select", "1", "2")
 
@@ -15,11 +15,9 @@ Towers <- c("Select", "1", "2")
 Tx15 <- read.csv("BMPTripod1_Min_15.dat")
 Tx15$TIMESTAMP <- ymd_hms(Tx15$TIMESTAMP)
 
-CalData <- read.csv("TestCalData.csv")
-CalData$TIMESTAMP <- ymd_hms(CalData$TIMESTAMP)
+#CalData <- read.csv("TestCalData.csv")
 
 Tx15flt <- Tx15
-CalDataflt <- CalData
 
 # User Interface Definition 
 ui <- fluidPage(
@@ -48,19 +46,20 @@ ui <- fluidPage(
       textInput("toDate", "To:", value = "2024-05-01 00:00:00" ),
          
       #1st Parameter
-      selectInput("WQP1", "Water Quality Parameter 1:", WQlist), 
+      selectInput("WQP1", "Water Quality Parameter 1 (x):", WQlist), 
       #2nd Parameter
-      selectInput("WQP2", "Water Quality Parameter 2:", WQlist),
-      actionButton("action", "Plot"),
+      selectInput("WQP2", "Water Quality Parameter 2 (Y):", WQlist),
+      actionButton("plotParams", "Plot"),
       width = 2,
-      
     ),
     
     mainPanel(
       width = 9,
       #Tabs
       tabsetPanel(
-        tabPanel("Plot", plotOutput("")),
+        tabPanel("Plot", plotOutput("plot")),
+      
+        
         tabPanel("Table", tableOutput("VarTable")), 
         tabPanel("Camera Feed",imageOutput("")), 
         tabPanel("Calibration Data", 
@@ -80,20 +79,49 @@ ui <- fluidPage(
 # Server Definition
 server <- shinyServer(function(input, output, session) {
   
-#Filter to selected date range:
-  rangeDates <- reactive({
-    Tx15flt <- filter(Tx15, between(Tx15$TIMESTAMP, 
-                        ymd_hms(input$fromDate),
-                        ymd_hms(input$toDate)
-                      ))
+#Plot two selected variables
+  parameterPlot <- reactive({
+    Tx15flt <- filter(Tx15, between(Tx15$TIMESTAMP,
+                                    ymd_hms(input$fromDate),
+                                    ymd_hms(input$toDate)
+    ))
     
-    CalDataflt <- filter(CalData, between(CalData$TIMESTAMP, 
-                        ymd_hms(input$fromDate),
-                        ymd_hms(input$toDate)
-                      ))
+    Tx15c <- Tx15flt[ ,c(input$WQP1,input$WQP2)]
+    
+    ggplot() +
+      geom_line(data = Tx15c, aes(x=Tx15c[,1],y=Tx15c[,2])) +
+      labs(title = "Plot", x=colnames(Tx15c)[1], y=colnames(Tx15c)[2]) +
+      theme_classic()
+    
+  })|> 
+    bindEvent(input$plotParams, ignoreNULL = FALSE)
+   
+  output$plot <- renderPlot({
+    parameterPlot()
+    
+  }) 
+    
+ #plot calibration plot
+  calibratePlot <- reactive({
+    #make this a function?
+    Tx15flt <- filter(Tx15, between(Tx15$TIMESTAMP, 
+                                    ymd_hms(input$fromDate),
+                                    ymd_hms(input$toDate)
+    )) 
+    
+    # file <- input$Calfile
+    # Calfile <- read.csv(file$datapath, header = input$header)
+    # print(Calfile)
+    # Calfile$TIMESTAMP <- ymd_hms(Calfile$TIMESTAMP)
+    # CalDataflt <- Calfile
+    
+    # CalDataflt <- filter(Calfile, between(Calfile$TIMESTAMP, 
+    #                     ymd_hms(input$fromDate),
+    #                     ymd_hms(input$toDate)
+    #                   ))
     ggplot() +
       geom_line(data = Tx15flt, aes(x=TIMESTAMP, y = NO3eq)) +
-      geom_line(data = CalDataflt, aes(x=TIMESTAMP, y = NO3conc), color = "red") +
+      #geom_line(data = CalDataflt, aes(x=TIMESTAMP, y = NO3conc), color = "red") +
     
       labs(title = "Nitrate Calibration Data at Station X") +
       theme_classic()
@@ -102,21 +130,14 @@ server <- shinyServer(function(input, output, session) {
     bindEvent(input$plotCalib, ignoreNULL = FALSE)
  
   
+  
+  
+  
   output$calib <- renderPlot({
-    rangeDates()
+    calibratePlot()
     
     })
 })
   
-
-    
-
-
-  
-
-  
-
-
-
 # End with call 
 shinyApp(ui = ui, server = server)
